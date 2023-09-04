865d7b4db01092602f93bcf6651ec8e7c1d2bc4f
AnarBat5 <99975652+AnarBat5@users.noreply.github.com>
2023-09-04 08:38:05 +0000

    HOHO

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,7 +24,7 @@ const Page = () =>{
     <Typography variant = "h3">h3</Typography>git
     <Typography variant = "schoolTitle">school title</Typography>
     <Typography variant = "body">Hot boy Anar</Typography>
-    <font size="+1638"><p>Hot boy Anar</p></font>
+    <p style="font-size:500px;">Hot boy Anar</p>
     <Typography variant = "bodyBold">bodyBold</Typography>
     <Typography variant = "caption">caption</Typography>
     <Typography variant = "captionUnderlined">body</Typography>
